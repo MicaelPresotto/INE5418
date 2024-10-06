e2af3eb3e3ee2a2a0cf819653b78f73bb8690a46
MicaelPresotto <mikal2003.sp@gmail.com>
2024-10-05 21:24:18 -0300

    lendo arquivo de conf

--- src/main.py
+++ src/main.py
@@ -9,29 +9,28 @@ class Vertex:
 
 class Graph:
     def __init__(self):
-        self.adjacency_list = {}
-        self.vertices = {}
+        self._adjacency_list = {}
+        self._vertices = {}
 
     def add_vertex(self, vertex: Vertex):
-        if vertex.id not in self.adjacency_list:
-            self.adjacency_list[vertex.id] = []
-            self.vertices[vertex.id] = vertex
+        if vertex.id not in self._adjacency_list:
+            self._adjacency_list[vertex.id] = []
+            self._vertices[vertex.id] = vertex
 
     def add_edge(self, u_id: int, v_id: int):
-        # Verifica se a aresta já existe antes de adicionar
-        if v_id not in self.adjacency_list[u_id]:
-            self.adjacency_list[u_id].append(v_id)
-        if u_id not in self.adjacency_list[v_id]:
-            self.adjacency_list[v_id].append(u_id)
+        if v_id not in self._adjacency_list[u_id]:
+            self._adjacency_list[u_id].append(v_id)
+        if u_id not in self._adjacency_list[v_id]:
+            self._adjacency_list[v_id].append(u_id)
 
     def get_vertex(self, id: int):
-        return self.vertices.get(id, None)
+        return self._vertices.get(id, None)
 
     def __str__(self):
         graph_str = ""
-        for vertex_id, neighbors in self.adjacency_list.items():
-            vertex_info = self.vertices[vertex_id]
-            neighbors_info = [self.vertices[n] for n in neighbors]
+        for vertex_id, neighbors in self._adjacency_list.items():
+            vertex_info = self._vertices[vertex_id]
+            neighbors_info = [self._vertices[n] for n in neighbors]
             graph_str += f"{vertex_info} -> {neighbors_info}\n"
         return graph_str
 
@@ -40,23 +39,30 @@ if __name__ == "__main__":
     graph = Graph()
     
     for line in file:
-        new_line = line.strip().split(':')  # Separar pelo ':'
-        vertex_id = int(new_line[0].strip())  # Capturar o ID do vértice
-        edges = new_line[1].strip().split(',')  # Capturar as conexões e remover espaços
-        
-        # Criar o vértice (se não existir)
-        if vertex_id not in graph.vertices:
+        new_line = line.strip().split(':')
+        vertex_id = int(new_line[0].strip())
+        edges = new_line[1].strip().split(',')
+    
+        if vertex_id not in graph._vertices:
             vertex = Vertex(id=vertex_id)
             graph.add_vertex(vertex)
         
-        # Adicionar as arestas (conexões)
         for edge in edges:
-            neighbor_id = int(edge.strip())  # Remover espaços e converter em int
-            if neighbor_id not in graph.vertices:
+            neighbor_id = int(edge.strip())
+            if neighbor_id not in graph._vertices:
                 neighbor_vertex = Vertex(id=neighbor_id)
                 graph.add_vertex(neighbor_vertex)
             graph.add_edge(vertex_id, neighbor_id)
-    
     file.close()
+
+    config = open("exemplo/config.txt", "r")
+    for line in config:
+        new_line = line.strip().split(' ')
+        vertex_id = int(new_line[0].replace(":", ""))
+        vertex = graph.get_vertex(vertex_id)
+        vertex.ip = new_line[1].replace(",", "")
+        vertex.porta = int(new_line[2].replace(",", ""))
+        vertex.banda = int(new_line[3].replace(",", ""))
+    config.close()
+
     print(graph)
-    print(graph.adjacency_list)
